4ca089afeaf0151c2c931390673d0ad6260e701b
Alex Thorne <alex@thorne.cc>
2017-12-23 13:03:27 +0000

    Split Babel and browsers compatibility into separate files

--- webpack.common.js
+++ webpack.common.js
@@ -2,39 +2,26 @@ const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const distDir = path.join(__dirname, 'dist')
+const dist = path.join(__dirname, 'dist')
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: [ './src/index.js' ]
   },
   output: {
     filename: '[name].bundle.js',
-    path: distDir
+    path: dist
   },
   module: {
     loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-stage-2',
-              '@babel/preset-react'
-            ]
-          }
-        }
-      },
+      { test: /\.jsx?$/, exclude: /node_modules/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
       { test: /\.(jpg|png|gif|svg|tiff|ico)$/, use: 'file-loader' },
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' }
     ]
   },
   plugins: [
-    new CleanWebpackPlugin([distDir]),
+    new CleanWebpackPlugin([dist]),
     new HtmlWebpackPlugin({
       title: 'Webpack Configurator',
       inject: 'body'
