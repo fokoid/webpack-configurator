1e41f937389dacb330709241de6ff4c8acc5fc9f
Alex Thorne <alex@thorne.cc>
2017-12-29 00:06:23 +0000

    Display real file content in FileViewer if possible
    Refactor FileViewer into class component to use lifecycle methods to
    handle fetch.

--- src/components/file-viewer.jsx
+++ src/components/file-viewer.jsx
@@ -1,49 +1,74 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {
   Button,
   Typography,
   ExpansionPanel,
   ExpansionPanelSummary,
   ExpansionPanelDetails,
-  ExpansionPanelActions
+  ExpansionPanelActions,
+  withStyles
 } from 'material-ui'
 import { ExpandMore as ExpandMoreIcon } from 'material-ui-icons'
 import { CopyToClipboard} from 'react-copy-to-clipboard'
+import urlJoin from 'url-join'
+
+const styles = {
+  name: {
+    flex: 1
+  }
+}
 
 //TODO Implement downloading of file content
 //TODO Implement opening file in new tab
-const FileViewer = ({ title, content, filename, callbacks }) => (
-  <ExpansionPanel defaultExpanded>
-    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-      <Typography type='subheading'>{title}</Typography>
-    </ExpansionPanelSummary>
-    <ExpansionPanelDetails>
-      <Typography>
-        <code>{filename}</code>
-        <br />
-        {content}
-      </Typography>
-    </ExpansionPanelDetails>
-    <ExpansionPanelActions>
-      <Button
-        dense
-        color='primary'
-        onClick={() => { console.log('Downloads not yet implemented.') }}
-      >Download</Button>
-      <Button
-        dense
-        color='primary'
-        onClick={() => { console.log('Opening in new tab not yet implemented.') }}
-      >Open</Button>
-      <CopyToClipboard text={content}>
+class FileViewer extends Component {
+  state = {
+    content: this.props.content || 'Loading...'
+  }
+
+  fileUrl = urlJoin(this.props.baseurl, this.props.filename)
+
+  componentDidMount = () => {
+    console.log(this.fileUrl)
+    fetch(this.fileUrl)
+    .then(res => res.text())
+    .then(text => this.setState({ content: text }))
+      .catch(err => console.log(err))
+  }
+
+  render = () => (
+    <ExpansionPanel defaultExpanded>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography type='subheading'>
+          <span className={this.props.classes.name}>
+            {this.props.title}
+          </span>
+          <code>{this.props.filename}</code>
+        </Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>{this.state.content}</Typography>
+      </ExpansionPanelDetails>
+      <ExpansionPanelActions>
         <Button
           dense
           color='primary'
-          onClick={callbacks.copy}
-        >Copy</Button>
-      </CopyToClipboard>
-    </ExpansionPanelActions>
-  </ExpansionPanel>
-)
+          onClick={() => { console.log('Downloads not yet implemented.') }}
+        >Download</Button>
+        <Button
+          dense
+          color='primary'
+          onClick={() => { console.log('Opening in new tab not yet implemented.') }}
+        >Open</Button>
+        <CopyToClipboard text={this.props.content}>
+          <Button
+            dense
+            color='primary'
+            onClick={this.props.callbacks.copy}
+          >Copy</Button>
+        </CopyToClipboard>
+      </ExpansionPanelActions>
+    </ExpansionPanel>
+  )
+}
 
-export default FileViewer
+export default withStyles(styles)(FileViewer)
