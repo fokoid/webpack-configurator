d91e10cb00757b06d2da738ea89214eb024b8959
Alex Thorne <alex@thorne.cc>
2018-01-05 16:29:24 +0000

    Add bundle analyzer webpack plugin

--- webpack/post.js
+++ webpack/post.js
@@ -1,8 +1,10 @@
 const path = require('path'),
+      BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin,
       WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = ({src, dist}) => ({
   plugins: [
+    new BundleAnalyzerPlugin(),
     new WorkboxWebpackPlugin({
       swSrc: path.join(src, 'sw.js'),
       swDest: path.join(dist, 'sw.js')
