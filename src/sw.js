3bbe7e5a570fc742e6fa8a29e65b356730288963
Alex Thorne <alex@thorne.cc>
2018-01-05 16:29:44 +0000

    WIP: service worker

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,33 @@
+// placeholder stuff
+const files = [
+  { name: 'webpack.config.js', language: 'javascript', content: 'module.exports = {}'},
+  { name: '.babelrc', language: 'json', content: '{}' },
+  { name: 'package.json', language: 'json', content: '{}' }
+]
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
+workbox.routing.registerRoute(
+  '/config/manifest',
+  () => new Response(
+    JSON.stringify({files}),
+    {
+      status: 200,
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }
+  )
+)
+
+workbox.routing.registerRoute(
+  /config\/files\/(.*)/,
+  ({url}) => {
+    const filename = url.pathname.match(/\/config\/files\/(.*)/)[1]
+    return new Response(
+      files
+      .filter(file => filename === file.name)
+      .map(({content}) => content)
+    )
+  }
+)
